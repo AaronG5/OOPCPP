f9f0bb903fec64c20053abe84c5e710176e393c3
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-02-24 00:42:17 +0200

    Add private member variables & constructor to ChessPiece class
    - Defined private member variables: type, isWhite, isAlive, xPos, yPos
    - Implemented class constructor

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -3,11 +3,26 @@
 using namespace std;
 
 class ChessPiece {
+   private:
+   const string type;
+   const bool isWhite;
+   bool isAlive;
+   char xPos;
+   int yPos;
 
+   public:
+   ChessPiece(string type, bool isWhite, bool isAlive, char xPos, int yPos) : 
+      type(type),
+      isWhite(isWhite), 
+      isAlive(isAlive), 
+      xPos(xPos), 
+      yPos(yPos) {}
 
 };
 
 int main() {
+
+   ChessPiece rook = ChessPiece("rook", true, true, 'a', 1);
 
    return 0;
 }
