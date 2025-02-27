499ef20b40b54c5db572dde4635216758c0d9ae1
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-02-27 18:08:32 +0200

    Add set & get methods
    - Added get methods to class fields
    - Added set methods
    - Added toString method

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 class ChessPiece {
    private:
+   static inline int amountOfObjects = 0;
+   static inline int ID = 0;
+   int id;
    const string type;
    const bool isWhite;
    bool isAlive;
@@ -12,11 +15,62 @@ class ChessPiece {
 
    public:
    ChessPiece(string type, bool isWhite, bool isAlive, char xPos, int yPos) : 
-      type(type),
-      isWhite(isWhite), 
-      isAlive(isAlive), 
-      xPos(xPos), 
-      yPos(yPos) {}
+   id(ID++), 
+   type(type), 
+   isWhite(isWhite), 
+   isAlive(isAlive), 
+   xPos(xPos), 
+   yPos(yPos) {
+      ++amountOfObjects;
+   }
+   
+   static int getAmountOfObjects() {
+      return amountOfObjects;
+   }
+   
+   int getId() {
+      return id;
+   }
+
+   private:
+   // const string setType(const string type) {
+   //    this->type = type;
+   // }
+
+   public:
+   string getType(string type) const {
+      return type;
+   }
+
+   bool getIsWhite() const {
+      return isWhite;
+   }
+
+   bool getIsAlive() {
+      return isAlive;
+   }
+   void setIsAlive(bool isAlive) {
+      this->isAlive = isAlive;
+   }
+
+   char getXPos() {
+      return xPos;
+   }
+   void setXPos(char xPos) {
+      this->xPos = xPos;
+   } 
+
+   int getYPos() {
+      return yPos;
+   }
+   void setYPos(int yPos) {
+      this->yPos = yPos;
+   }
+   
+   string toString() {
+      return "ID: " + to_string(id) + "; type: " + type + "; isWhite: " + to_string(isWhite) + "; isAlive: " + to_string(isAlive) + "; xPosition: " + xPos + "; yPosition: " + to_string(yPos) + "\n";
+   }
+
 
 };
 
