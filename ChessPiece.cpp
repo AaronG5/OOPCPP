d2ba664b63343d70842750c666a9ab42ab0f34bc
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-02-28 01:36:00 +0200

    Add class action methods & input validation
    - Added input validation for <string validateType>, <void setXPos> & <void setYPos>
    - Added class methods <void killPiece> & <void movePiece>
    - Added destructor.
    - Implemented <ChessPiece *chessPieceArray[]> for storing <class ChessPiece> objects in <int main()>
    - Added <try{} catch{}> in <main()> for exception handling and error control when validating input.

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <cctype>
 
 using namespace std;
 
 class ChessPiece {
    private:
-   static inline int amountOfObjects = 0;
-   static inline int ID = 0;
    int id;
    const string type;
    const bool isWhite;
    bool isAlive;
    char xPos;
    int yPos;
-
+   
    public:
+   static int amountOfObjects;
+   static int ID;
+
    ChessPiece(string type, bool isWhite, bool isAlive, char xPos, int yPos) : 
    id(ID++), 
-   type(type), 
+   type(validateType(type)), 
    isWhite(isWhite), 
-   isAlive(isAlive), 
-   xPos(xPos), 
-   yPos(yPos) {
+   isAlive(isAlive) {
       ++amountOfObjects;
+      setXPos(xPos);
+      setYPos(yPos);
+   }
+
+   ~ChessPiece() {
+      --amountOfObjects;
    }
    
    static int getAmountOfObjects() {
@@ -31,17 +39,53 @@ class ChessPiece {
    int getId() {
       return id;
    }
-
-   private:
-   // const string setType(const string type) {
-   //    this->type = type;
-   // }
-
-   public:
+   
    string getType(string type) const {
       return type;
    }
 
+   private:
+   string validateType(string type) const{
+      for(char &c : type) {
+         c = tolower(c);
+      }
+      if(type == "king" || type == "queen" || type == "rook" 
+         || type == "bishop" || type == "knight" || type == "pawn") {
+         return type;
+      }
+      else {
+         throw(runtime_error("Invalid input.\n"));
+      }
+   }
+
+   void setIsAlive(bool isAlive) {
+      this->isAlive = isAlive;
+   }
+   
+   void setXPos(char xPos) {
+      xPos = tolower(xPos);
+      // (xPos >= 'a' && xPos <= 'h') 
+      // ? this->xPos = xPos 
+      // : throw(runtime_error("Invalid input.\n"));
+      if(xPos >= 'a' && xPos <= 'h') {
+         this->xPos = xPos;
+      }
+      else {
+         throw(runtime_error("Invalid input.\n"));
+      }
+   } 
+   
+   void setYPos(int yPos) {
+      // (yPos > 0 && yPos < 9) ? this->yPos = yPos : throw(runtime_error("Invalid input.\n"));
+      if(yPos > 0 && yPos < 9) {
+         this->yPos = yPos;
+      }
+      else {
+         throw(runtime_error("Invalid input.\n"));
+      }
+   }
+
+   public:
    bool getIsWhite() const {
       return isWhite;
    }
@@ -49,34 +93,63 @@ class ChessPiece {
    bool getIsAlive() {
       return isAlive;
    }
-   void setIsAlive(bool isAlive) {
-      this->isAlive = isAlive;
-   }
 
    char getXPos() {
       return xPos;
    }
-   void setXPos(char xPos) {
-      this->xPos = xPos;
-   } 
 
    int getYPos() {
       return yPos;
    }
-   void setYPos(int yPos) {
-      this->yPos = yPos;
-   }
    
    string toString() {
-      return "ID: " + to_string(id) + "; type: " + type + "; isWhite: " + to_string(isWhite) + "; isAlive: " + to_string(isAlive) + "; xPosition: " + xPos + "; yPosition: " + to_string(yPos) + "\n";
+      return "ID: " + to_string(id) + "; type: " + type + "; isWhite: " + to_string(isWhite) + "; isAlive: " 
+      + to_string(isAlive) + "; xPosition: " + xPos + "; yPosition: " + to_string(yPos) + "\n";
    }
 
+   void killPiece() {
+      setIsAlive(false);
+   }
 
+   void movePiece(char xPos, int yPos) {
+      setXPos(xPos);
+      setYPos(yPos);
+   }
 };
+
+int ChessPiece::amountOfObjects = 0;
+int ChessPiece::ID = 0;
 
 int main() {
 
-   ChessPiece rook = ChessPiece("rook", true, true, 'a', 1);
+   try {
+
+      ChessPiece *chessPieceArray[5];
+      chessPieceArray[0] = new ChessPiece("rook", true, true, 'a', 1);
+      cout << chessPieceArray[0]->toString();
+      cout << ChessPiece::getAmountOfObjects() << "\n";
+
+
+      chessPieceArray[1] = new ChessPiece("ROOK", false, true, 'H', 8);
+      cout << chessPieceArray[1]->toString();
+      cout << ChessPiece::getAmountOfObjects() << "\n";
+
+      chessPieceArray[0]->movePiece('a', 6);
+      cout << chessPieceArray[0]->toString();
+
+      chessPieceArray[0]->killPiece();
+      cout << chessPieceArray[0]->toString();
+      
+      delete chessPieceArray[0];
+      cout << chessPieceArray[0]->toString();
+
+   }
+   catch(runtime_error(e)) {
+      cout << e.what();
+   }
+   catch(...) {
+      cout << "Unexpected error!\n";
+   }
 
    return 0;
 }
