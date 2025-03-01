eefdb7d895c26289fceeffb05bede5f1867a476f
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-03-01 21:59:15 +0200

    Change based on prof. request
    - Changed <string type> to <enum pieceType type> for easier validation and effectiveness.
    - Made <static int amountOfObjects> & <static int ID> private fields.
    - Added new constructor for initialization of chess piece object without coordinates (<char xPos> & <int yPos>).
    - Changed constructor logic. <bool isAlive> now is initialized based on the constructor that is being used. If object is constructed without coordinates, isAlive = false and vice versa.
    - Added conditional statements to <string toString>
    - Added funtion <void deleteArray> for deletion of created objects.
    - Moved "assert(ChessPiece::getAmountOfObjects() == 0);" to the end of program for better testing.

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -4,54 +4,62 @@
 #include <cctype>
 #include <assert.h>
 
+#define ARRAY_SIZE 5
+
 using namespace std;
+
+enum pieceType {
+   PAWN,
+   KNIGHT,
+   BISHOP,
+   ROOK,
+   QUEEN,
+   KING
+};
 
 class ChessPiece {
    private:
+   static int amountOfObjects;
+   static int ID;
    int id;
-   const string type;
+   const enum pieceType type;
    const bool isWhite;
    bool isAlive;
    char xPos;
    int yPos;
-   
-   public:
-   static int amountOfObjects;
-   static int ID;
 
-   ChessPiece(string type, bool isWhite, bool isAlive, char xPos, int yPos) : 
-   id(ID++), 
-   type(validateType(type)), 
-   isWhite(isWhite), 
-   isAlive(isAlive) {
+   public:
+   ChessPiece(enum pieceType type, bool isWhite) 
+      : id(ID++), 
+      type(type), 
+      isWhite(isWhite)
+   {  
+      setIsAlive(false);
       ++amountOfObjects;
+   }
+
+   ChessPiece(enum pieceType type, bool isWhite, char xPos, int yPos) 
+      : id(ID++), 
+      type(type), 
+      isWhite(isWhite) 
+   {
+      setIsAlive(true);
       setXPos(xPos);
       setYPos(yPos);
+      ++amountOfObjects;
    }
-
+   
+   ~ChessPiece() {
+      --amountOfObjects;
+   }
+   
    private:
-   string validateType(string type) const{
-      for(char &c : type) {
-         c = tolower(c);
-      }
-      if(type == "king" || type == "queen" || type == "rook" 
-         || type == "bishop" || type == "knight" || type == "pawn") {
-         return type;
-      }
-      else {
-         throw(runtime_error("Error. Not a valid chess piece type.\n"));
-      }
-   }
-
    void setIsAlive(bool isAlive) {
       this->isAlive = isAlive;
    }
    
    void setXPos(char xPos) {
       xPos = tolower(xPos);
-      // (xPos >= 'a' && xPos <= 'h') 
-      // ? this->xPos = xPos 
-      // : throw(runtime_error("Invalid input.\n"));
       if(xPos >= 'a' && xPos <= 'h') {
          this->xPos = xPos;
       }
@@ -61,7 +69,6 @@ class ChessPiece {
    } 
    
    void setYPos(int yPos) {
-      // (yPos > 0 && yPos < 9) ? this->yPos = yPos : throw(runtime_error("Invalid input.\n"));
       if(yPos > 0 && yPos < 9) {
          this->yPos = yPos;
       }
@@ -71,10 +78,6 @@ class ChessPiece {
    }
 
    public:
-   ~ChessPiece() {
-      --amountOfObjects;
-   }
-   
    static int getAmountOfObjects() {
       return amountOfObjects;
    }
@@ -83,7 +86,7 @@ class ChessPiece {
       return id;
    }
    
-   string getType() const {
+   enum pieceType getType() const {
       return type;
    }
 
@@ -103,94 +106,118 @@ class ChessPiece {
       return yPos;
    }
    
-   string toString() {
-      return "ID: " + to_string(id) + "; type: " + type + "; isWhite: " + to_string(isWhite) + "; isAlive: " 
-      + to_string(isAlive) + "; xPosition: " + xPos + "; yPosition: " + to_string(yPos) + "\n";
-   }
-
-   void killPiece() {
+   void kill() {
       setIsAlive(false);
    }
+   
+   void move(char xPos, int yPos) {
+      if(isAlive) {
+         setXPos(xPos);
+         setYPos(yPos);
+      }
+      else {
+         throw runtime_error("Error. Can't change coordinates of a dead chess piece.\n");
+      }
+   }
 
-   void movePiece(char xPos, int yPos) {
-      setXPos(xPos);
-      setYPos(yPos);
+   string toString() {
+      if(isAlive) {
+         return to_string(id) + " " + to_string(type) + " " + to_string(isWhite) + " " 
+         + to_string(isAlive) + " " + xPos + " " + to_string(yPos) + "\n";
+      }
+      else {
+         return to_string(id) + " " + to_string(type) + " " + to_string(isWhite) + " " 
+         + to_string(isAlive) + "\n";
+      }
    }
 };
 
 int ChessPiece::amountOfObjects = 0;
 int ChessPiece::ID = 0;
 
+void deleteArray(ChessPiece **array) {
+   int size = ChessPiece::getAmountOfObjects();
+   for (int i = 0; i < size; ++i) {
+      delete array[i];
+   }
+}
+
 int main() {
+   
+   ChessPiece *chessPieceArray[ARRAY_SIZE];
 
    try {
 
-      ChessPiece *chessPieceArray[5];
-
-      chessPieceArray[0] = new ChessPiece("rook", true, true, 'a', 1);
+      chessPieceArray[0] = new ChessPiece(ROOK, true, 'a', 1);
       assert(chessPieceArray[0]->getId() == 0);
-      assert(chessPieceArray[0]->getType() == "rook");
+      assert(chessPieceArray[0]->getType() == ROOK);
       assert(chessPieceArray[0]->getIsAlive());
       assert(chessPieceArray[0]->getIsWhite());
       assert(chessPieceArray[0]->getXPos() == 'a');
       assert(chessPieceArray[0]->getYPos() == 1);
       assert(ChessPiece::getAmountOfObjects() == 1);
 
-      chessPieceArray[1] = new ChessPiece("ROOK", false, true, 'H', 8);
+      chessPieceArray[1] = new ChessPiece(ROOK, false, 'H', 8);
       assert(chessPieceArray[1]->getId() == 1);
-      assert(chessPieceArray[1]->getType() == "rook");
+      assert(chessPieceArray[1]->getType() == ROOK);
       assert(chessPieceArray[1]->getIsAlive());
       assert(!chessPieceArray[1]->getIsWhite());
       assert(chessPieceArray[1]->getXPos() == 'h');
       assert(chessPieceArray[1]->getYPos() == 8);
       assert(ChessPiece::getAmountOfObjects() == 2);
 
-      chessPieceArray[0]->movePiece('a', 6);
+      chessPieceArray[0]->move('a', 6);
       assert(chessPieceArray[0]->getId() == 0);
-      assert(chessPieceArray[0]->getType() == "rook");
+      assert(chessPieceArray[0]->getType() == ROOK);
       assert(chessPieceArray[0]->getIsAlive());
       assert(chessPieceArray[0]->getIsWhite());
       assert(chessPieceArray[0]->getXPos() == 'a');
       assert(chessPieceArray[0]->getYPos() == 6); // Only this changes
       assert(ChessPiece::getAmountOfObjects() == 2);
 
-      chessPieceArray[0]->killPiece();
+      chessPieceArray[0]->kill();
       assert(chessPieceArray[0]->getId() == 0);
-      assert(chessPieceArray[0]->getType() == "rook");
+      assert(chessPieceArray[0]->getType() == ROOK);
       assert(!chessPieceArray[0]->getIsAlive()); // Only this changes
       assert(chessPieceArray[0]->getIsWhite());
       assert(chessPieceArray[0]->getXPos() == 'a');
       assert(chessPieceArray[0]->getYPos() == 6);
       assert(ChessPiece::getAmountOfObjects() == 2);
 
-      chessPieceArray[2] = new ChessPiece("kInG", true, true, 'd', 3);
+      chessPieceArray[2] = new ChessPiece(KING, true, 'd', 3);
       assert(chessPieceArray[2]->getId() == 2);
-      assert(chessPieceArray[2]->getType() == "king");
+      assert(chessPieceArray[2]->getType() == KING);
       assert(chessPieceArray[2]->getIsAlive());
       assert(chessPieceArray[2]->getIsWhite());
       assert(chessPieceArray[2]->getXPos() == 'd');
       assert(chessPieceArray[2]->getYPos() == 3);
       assert(ChessPiece::getAmountOfObjects() == 3);
+
+      chessPieceArray[3] = new ChessPiece(KNIGHT, true);
+      assert(chessPieceArray[3]->getId() == 3);
+      assert(chessPieceArray[3]->getType() == KNIGHT);
+      assert(!(chessPieceArray[3]->getIsAlive()));
+      assert(chessPieceArray[3]->getIsWhite());
+      assert(ChessPiece::getAmountOfObjects() == 4);
       
       cout << chessPieceArray[2]->toString();
-      
-      delete chessPieceArray[0];
-      assert(ChessPiece::getAmountOfObjects() == 2);
-      delete chessPieceArray[1];
-      assert(ChessPiece::getAmountOfObjects() == 1);
-      delete chessPieceArray[2];
-      assert(ChessPiece::getAmountOfObjects() == 0);
+      cout << chessPieceArray[3]->toString();
 
-
-      chessPieceArray[3] = new ChessPiece("queef", 5, true, 'd', 3);
+      chessPieceArray[4] = new ChessPiece(QUEEN, true, 'k', 87);
       cout << "Foobar\n"; // Shouldn't be reachable
+
+      deleteArray(chessPieceArray);
    }
    catch(runtime_error(e)) {
       cout << e.what();
+      deleteArray(chessPieceArray);
    }
    catch(...) {
       cout << "Unexpected error!\n";
+      deleteArray(chessPieceArray);
    }
+
+   assert(ChessPiece::getAmountOfObjects() == 0);
 
    return 0;
 }
