bbe97bee709d51c7433f3cf751648a218e6c3582
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-04-06 18:52:26 +0300

    Update `test.cpp` accordingly to test new methods

--- IP2_Hash_Table/test.cpp
+++ IP2_Hash_Table/test.cpp
@@ -1,5 +1,6 @@
 #include "header.h"
 #include <cassert>
+#include <iostream>
 
 using namespace myHash;
 
@@ -8,34 +9,62 @@ int main() {
    HashTable test1(amount);
    // HashTable *test1 = new HashTable(amount);
    
-   // hashFunction test
-   assert(test1.hashFunction("Aa") == 2);
-   assert(test1.hashFunction("Tim") == 8);
+   try {
+      
+      // insert test
+      test1.insert("Tim", 20); // ASCII value = 298
+      test1.insert("Adam", 30); // ASCII value = 371
+      test1.insert("Jin", 73); // ASCII value = 289
+      test1.insert("Jiw", 60); // ASCII value = 298
    
-   // insert test
-   test1.insert("Tim", 20); // ASCII value = 298
-   test1.insert("Adam", 30); // ASCII value = 371
-   test1.insert("Jim", 73); // ASCII value = 288
-   test1.insert("Jiw", 60); // ASCII value = 298
-
-   assert(test1.numOfObjects() == 4);
-
-   // find test
-   assert(test1.find("Tim") == 20);
-   assert(test1.find("Adam") == 30);
-   assert(test1.find("Jim") == 73);
-   assert(test1.find("Jiw") == 60);
-   assert(test1.find("John") == -1);
-
-
-   // delete test
-   test1.remove("Tim");
-   test1.remove("Adam");
-   test1.remove("Jim");
-   test1.remove("Jiw");
+      assert(test1.getSize() == 4);
    
-   assert(test1.numOfObjects() == 0);
+      // copy test
+      HashTable testCopy(test1);
+      assert(testCopy.find("Tim") == 20);
+      assert(testCopy.find("Adam") == 30);
+      assert(testCopy.find("Jin") == 73);
+      assert(testCopy.find("Jiw") == 60);
+   
+      // find test
+      assert(test1.find("Tim") == 20);
+      assert(test1.find("Adam") == 30);
+      assert(test1.find("Jin") == 73);
+      assert(test1.find("Jiw") == 60);
+   
+      // operator[] test (insertion)
+      assert(test1["Tim"] == 20);
+      assert(test1["Adam"] == 30);
+      assert(test1["Jin"] == 73);
+      assert(test1["Jiw"] == 60);
+   
+      std::cout << test1.toString();
+   
+      // operator== test (comparison)
+      assert(test1 == testCopy);
+   
+      // delete test
+      // test1.remove("Tim");
+      // test1.remove("Adam");
+      // test1.remove("Jin");
+      // test1.remove("Jiw");
+   
+      // operator! test (clear table)
+      !test1;
+      
+      assert(test1.getSize() == 0);
 
-   printf("Hi!\n");
+      // exception test
+      testCopy.find("John");
+      assert(false); // shouldn't be reached
+   } 
+   catch (const std::exception& e) {
+      std::cout << e.what() << "\n";
+   } 
+   catch (...) {
+      std::cout << "Unknown error occurred\n";
+   }
+
+   printf("Unit testing passed!\n");
    return 0;
 }
