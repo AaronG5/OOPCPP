aa2ebb50d7aad26274fd8bffa2cd48b646105612
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-04-07 02:54:57 +0300

    Add documentation about class structure and usage

--- IP2_Hash_Table/header.h
+++ IP2_Hash_Table/header.h
@@ -1,6 +1,39 @@
-// Aaron Gandžumian
-// 01-04-2025
-// aaron.gandzumian@mif.stud.vu.lt
+/* 
+Aaron Gandžumian PS 2gr. 01-04-2025
+ID: 2413934
+aaron.gandzumian@mif.stud.vu.lt
+
+This header file defines the implementation of a hash table using separate chaining.
+
+The hash table stores key-value pairs and uses a vector of linked lists (std::vector of std::list of std::pair) to handle collisions.
+   Key = string; Value = int; Can be adapted for any kind of data.
+
+Collision resolution: When two key-value pairs are trying to access the same index value, they are instead stored in the same list (separate chaining).
+Hash table resizing and rehashing: hash table automatically increases capacity by a factor of 2 once size == capacity
+
+Primary hash table functions (all lookups, deletions, etc. are done using key):
+- Insert key-value pair
+- Remove key-value pair
+- Find value by key
+- Resize table and rehash key-value pairs
+
+- Operator overloads:
+   - `!` clear table of all key-value pairs and free allocated memory of data
+   - `()` insert key-value pair
+   - `[]` find value by key
+   - `==` compare whether two tables have the same content
+   - `>` compare whether one table is bigger than the other based on the number of elements
+
+Class provides basic exception handling through `KeyNotFoundException` class. Exception is thrown whenever key is not found during lookup or removal.
+
+The implementation uses the PImpl (Pointer to Implementation) idiom for better separation of interface and implementation and maintaining
+principles of encapsulation.
+
+Deep copying of created hash tables is possible thanks to deep copy constructor.
+
+
+
+*/
 
 #ifndef HEADER_H
 #define HEADER_H
@@ -18,21 +51,22 @@ namespace myHash {
    };
 
    class HashTable {
-      private:
+   private:
       class Impl;
       std::unique_ptr<Impl> pImpl;
    
    public:
    HashTable(const int& amountOfBuckets);
-   HashTable(const HashTable& other); // Copy constructor
+   HashTable(const HashTable& other); // Deep copy constructor
+   HashTable& operator= (const HashTable& other); // Copy
    ~HashTable();
    
    int getSize() const; // Return number of objects in hash table
    int getCapacity() const;
-   void setCapacity(const int& capacity);
    void insert(const std::string& key, const int& value); // Insert key-value pair by key
    void remove(const std::string& key); // Remove key-value pair by key
    int find(const std::string& key) const; // Find value by key
+   void rehash(const int& newCapacity);
    
    void operator!(); // Clear whole table
    int operator[] (const std::string& key) const; // Search table by key, return value
@@ -41,8 +75,6 @@ namespace myHash {
    bool operator> (const HashTable& other) const; // Is greater than other table based on number of elements
    
    std::string toString() const;
-   
-   // TODO: Add iterator functions
    };
 }
 
