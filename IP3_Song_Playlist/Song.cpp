c862734863205280fbfc0935317cc3a1d4f76683
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-05-09 15:39:25 +0300

    Create `Song` class

--- IP3_Song_Playlist/Song.cpp
+++ IP3_Song_Playlist/Song.cpp
@@ -1,1 +1,18 @@
+#include "Song.h"
 
+int Song::nextId = 0;
+
+Song::Song(const std::string& title, const std::string& artist, const int& length) : 
+   title(title), 
+   artist(artist), 
+   length(length) 
+{
+   uniqueId = nextId++;
+}
+
+Song::~Song() = default;
+
+int Song::getUniqueId() const { return uniqueId; }
+std::string Song::getTitle() const { return title; }
+std::string Song::getArtist() const { return artist; }
+int Song::getLength() const { return length; }
--- IP3_Song_Playlist/Song.h
+++ IP3_Song_Playlist/Song.h
@@ -1,1 +1,24 @@
+#ifndef SONG_H
+#define SONG_H
 
+#include <string>
+
+class Song {
+   private:
+   static int nextId;
+   int uniqueId;
+   std::string title;
+   std::string artist;
+   int length;
+
+   public:
+   Song(const std::string& title, const std::string& artist, const int& length);
+   ~Song();
+
+   int getUniqueId() const;
+   std::string getTitle() const;
+   std::string getArtist() const;
+   int getLength() const;
+};
+
+#endif
