e87f314e604da416b153bc59f77fa10615574c14
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-05-10 17:05:54 +0300

    Create declarations and definitions for `SongPlaylist` class
    - `SongPlaylist` dinamically stores list of `Song` object pointers for polymorthic capabilities and dynamic playlist manipulation.
    - `std::shared_ptr` is used for `Song` objects ir order to avoid object duplication when storing the same `Song` object in several different playlists.

--- IP3_Song_Playlist/SongPlaylist.cpp
+++ IP3_Song_Playlist/SongPlaylist.cpp
@@ -1,1 +1,34 @@
+#include "SongPlaylist.h"
 
+SongPlaylist::SongPlaylist() { 
+   playlist.resize(0); 
+   sorter = nullptr;
+}
+
+SongPlaylist::~SongPlaylist() = default;
+
+void SongPlaylist::addSong(const std::string& title, const std::string& artist, const int& length) {
+   playlist.push_back(std::make_shared<Song>(title, artist, length));
+   if(sorter != nullptr) {
+      sortPlaylist();
+   }
+}
+
+void SongPlaylist::removeSong(const std::string& title) {
+   for(auto it = playlist.begin(); it < playlist.end(); ++it) {
+      if((*it)->getTitle() == title) {
+         playlist.erase(it);
+         break;
+      }
+   }
+}
+
+std::vector<std::shared_ptr<Song>> SongPlaylist::getPlaylist() const { return playlist; }
+
+std::string SongPlaylist::getPlaylistInfo() const {
+   std::stringstream ss;
+   for(auto it = playlist.begin(); it < playlist.end(); ++it) {
+      ss << (*it)->getSongInfo() << std::endl;
+   }
+   return ss.str();
+}
--- IP3_Song_Playlist/SongPlaylist.h
+++ IP3_Song_Playlist/SongPlaylist.h
@@ -1,1 +1,32 @@
+#ifndef SONG_PLAYLIST_H
+#define SONG_PLAYLIST_H
 
+#include "Song.h"
+#include "SortingMethod.h"
+#include <vector>
+
+class SongPlaylist {
+   private: 
+   std::vector<std::shared_ptr<Song>> playlist;
+   std::shared_ptr<SortingMethod> sorter;
+
+   public:
+   SongPlaylist();
+   ~SongPlaylist();
+
+   void addSong(const std::string& title, const std::string& artist, const int& length); // Add song to playlist and automatically sort by sorting method
+
+   void removeSong(const std::string& title); // Remove song from playlist by title
+
+   void addSongToOtherPlaylist(const std::string& title, SongPlaylist other); // Add song from current playlist to other playlist by searching for song title
+
+   void sortPlaylist(); // sort playlist depending on sorting method
+
+   void setSortMethod();
+
+   std::vector<std::shared_ptr<Song>> getPlaylist() const;
+   std::string getPlaylistInfo() const; // return information for every song from playlist in order
+
+};
+
+#endif
