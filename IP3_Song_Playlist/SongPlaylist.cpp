6a675d5c08313b934dee9b0c647296c4b00e3adf
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-05-12 22:49:28 +0300

    Create several new methods and copy constructor
    - Separated Song and SongPlaylist class declarations into separate header files.
    - Added project description and student information
    - Created copy constructor for easy deep copying of playlist instances.
    - Created similiar `addSong` method which splits seconds into minutes and seconds for easier song input.
    - Created `operator=` method for deep copying.
    - Added throw statements to several methods for undefined behaviour prevention.

--- IP3_Song_Playlist/SongPlaylist.cpp
+++ IP3_Song_Playlist/SongPlaylist.cpp
@@ -1,18 +1,25 @@
 #include "SongPlaylist.h"
 
-SongPlaylist::SongPlaylist() { 
-   playlist.resize(0); 
-   sorter = nullptr;
+SongPlaylist::SongPlaylist(const SongPlaylist& other) {
+   this->playlist = other.playlist;
+   this->sorter = other.sorter ? other.sorter->clone() : nullptr;
 }
 
-void SongPlaylist::addSong(const std::string& title, const std::string& artist, const int& length) {
-   playlist.push_back(std::make_shared<Song>(title, artist, length));
+void SongPlaylist::addSong(const std::string& title, const std::string& artist, const int& s) {
+   playlist.push_back(std::make_shared<Song>(title, artist, s));
    if(sorter != nullptr) {
       sortPlaylist();
    }
 }
 
+void SongPlaylist::addSong(const std::string& title, const std::string& artist, const int& min, const int& s) {
+   addSong(title, artist, min * 60 + s);
+}
+
 void SongPlaylist::addSong(std::shared_ptr<Song> song) {
+   if(song == nullptr) {
+      throw std::invalid_argument("Song points to null");
+   }
    playlist.push_back(song);
    if(sorter != nullptr) {
       sortPlaylist();
@@ -23,41 +30,53 @@ void SongPlaylist::removeSong(const std::string& title) {
    for(auto it = playlist.begin(); it < playlist.end(); ++it) {
       if((*it)->getTitle() == title) {
          playlist.erase(it);
-         break;
+         return;
       }
    }
+   throw std::invalid_argument("Song not found: " + title);
 }
 
 void SongPlaylist::addSongToOtherPlaylist(const std::string& title, SongPlaylist& other) {
    other.addSong(this->getSong(title));
 }
 
+void SongPlaylist::setSortMethod(const int& which) {
+   switch(which) {
+      case SORT_BY_LENGTH:
+      sorter = std::make_unique<SortByLength>();
+      break;
+      case SORT_BY_TITLE:
+      sorter = std::make_unique<SortByTitle>();
+      break;
+      case SORT_BY_ARTIST:
+      // 3rd sorting method should go here
+      break;
+      default:
+      throw std::out_of_range("Value out of range");
+   }
+}
+
 void SongPlaylist::sortPlaylist() {
    if(sorter) { sorter->sortPlaylist(playlist); }
 }
 
-void SongPlaylist::setSortMethod(const int& which) {
-   switch(which) {
-      case SORT_BY_LENGTH:
-         sorter = std::make_unique<SortByLength>();
-         break;
-      case SORT_BY_TITLE:
-         sorter = std::make_unique<SortByTitle>();
-         break;
-      case SORT_BY_ARTIST:
-         // 3rd sorting method should go here
-         break;
-   }
+std::shared_ptr<Song> SongPlaylist::operator[] (const int& index) const {
+   if(index < playlist.size()) { return playlist[index]; }
+   else { throw std::out_of_range("Index value out of range"); }
 }
 
-std::shared_ptr<Song> SongPlaylist::operator[] (const std::string& title) const {
-   return getSong(title);
+SongPlaylist& SongPlaylist::operator= (const SongPlaylist& other) { 
+   if(this == &other) { return *this; }
+   this->playlist = other.playlist;
+   this->sorter = other.sorter ? other.sorter->clone() : nullptr;
+   return *this;
 }
 
 std::shared_ptr<Song> SongPlaylist::getSong(const std::string& title) const {
    for(auto it = playlist.begin(); it < playlist.end(); ++it) {
       if((*it)->getTitle() == title) { return *it; }
    }
+   throw std::invalid_argument("Song not found: " + title);
 }
 
 std::vector<std::shared_ptr<Song>> SongPlaylist::getPlaylist() const { return playlist; }
--- IP3_Song_Playlist/SongPlaylist.h
+++ IP3_Song_Playlist/SongPlaylist.h
@@ -1,32 +1,43 @@
+// Aaron Gandžumian PS 2gr. 12-05-2025
+// ID: 2413934
+// aaron.gandzumian@mif.stud.vu.lt
+
+// Structure of project:
+// # SongPlaylist - represents a playlist used for storing 'Song' objects inside of its vector.
+// # Song - represents a song that can be stored in 'SongPlaylist' instances.
+// # SortingMethod - fully virtual class used as a base class for different methods of sorting the playlist.
+// # SortByLength - method of sorting the playlist by song length using Tree Sort. Derived from 'SortingMethod'.
+// # SortByTitle - method of sorting the playlist by song title using Gnome Sort. Derived from 'SortingMethod'.
+// # SortByArtist - method of sorting the playlist by song artist. Derived from 'SortingMethod'. To be implemented by colleagues.
+
+// Methods included in 'SongPlaylist':
+// # Deep copy constructor
+// # 'addSong' - adds a song to the playlist, automatically sorting it by the currently set sorting method.
+// # 'removeSong' - removes a song from the playlist.
+// # 'addSongToOtherPlaylist' - copies a song from one playlist to another.
+// # 'setSortMethod' - set sort method which will be used to sort the playlist when calling 'sortPlaylist'.
+// # 'sortPlaylist' - sort the playlist by the method set using 'setSortMethod'. If no sorting method is set, the method won't do anything.
+// # 'operator[]' - returns a pointer to a song from the playlist by its index.
+// # 'operator=' - assigment operator override.
+// # 'getSong' - returns a pointer to a song from the playlist by its title.
+// # 'getPlaylist' - returns the vector which stores all the songs.
+// # 'getPlaylistInfo' - returns a formatted string of the contents of the playlist vector.
+
+// NOTE: EVERY SONG TITLE IS ASSUMED TO BE UNIQUE
+// It would be easy to resolve this issue by accessing Song objects by their unique ID instead, but 
+// for easier and more readable testing, Song objects are accessed by their titles.
+
 #ifndef SONG_PLAYLIST_H
 #define SONG_PLAYLIST_H
 
+#include "Song.h"
 #include "SortingMethod.h"
 #include <vector>
-#include <sstream>
+#include <stdexcept>
 
 #define SORT_BY_LENGTH 0
 #define SORT_BY_TITLE 1
 #define SORT_BY_ARTIST 2
-
-class Song {
-   private:
-   static int nextId;
-   int uniqueId;
-   std::string title;
-   std::string artist;
-   int length; // Seconds
-
-   public:
-   Song(const std::string& title, const std::string& artist, const int& length);
-
-   int getUniqueId() const;
-   std::string getTitle() const;
-   std::string getArtist() const;
-   int getLength() const;
-   std::string getSongInfo() const;
-};
-
 
 class SongPlaylist {
    private: 
@@ -34,25 +45,28 @@ class SongPlaylist {
    std::unique_ptr<SortingMethod> sorter;
 
    public:
-   SongPlaylist();
+   SongPlaylist() = default;
+   SongPlaylist(const SongPlaylist& other);
 
-   void addSong(const std::string& title, const std::string& artist, const int& length); // Create song and add to playlist, automatically sort by sorting method
+   void addSong(const std::string& title, const std::string& artist, const int& s); // Create song, add to playlist and sort
+   void addSong(const std::string& title, const std::string& artist, const int& min, const int& s); // Pass minutes and seconds as separate arguments
 
-   void addSong(std::shared_ptr<Song> song); // Pass song and add to playlist, automatically sort by sorting method
+   void addSong(std::shared_ptr<Song> song); // Pass song and add to playlist and sort
 
-   void removeSong(const std::string& title); // Remove song from playlist by title
+   void removeSong(const std::string& title);
 
-   void addSongToOtherPlaylist(const std::string& title, SongPlaylist& other); // Add song from current playlist to other playlist by searching for song title
-
-   void sortPlaylist(); // sort playlist depending on sorting method
+   void addSongToOtherPlaylist(const std::string& title, SongPlaylist& other);
 
    void setSortMethod(const int& which);
 
-   std::shared_ptr<Song> operator[] (const std::string& title) const; // Same as getSong
+   void sortPlaylist();
+
+   std::shared_ptr<Song> operator[] (const int& index) const; 
+   SongPlaylist& operator= (const SongPlaylist& other);
 
    std::shared_ptr<Song> getSong(const std::string& title) const;
    std::vector<std::shared_ptr<Song>> getPlaylist() const;
-   std::string getPlaylistInfo() const; // return information for every Song from playlist in order
+   std::string getPlaylistInfo() const; 
 };
 
 #endif
